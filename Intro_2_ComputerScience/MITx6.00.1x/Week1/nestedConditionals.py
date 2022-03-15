36c4812f2c18f11e4c9539bebec294342fa65a73
Iván E. Hernández T <iamieht@gmail.com>
2022-03-15 17:07:59 +0100

    Create nestedConditionals.py
    nested conditionals

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week1/nestedConditionals.py
@@ -0,0 +1,8 @@
+x = int(input("Enter an integer: "))
+if x%2 == 0:
+    if x%3 == 0:
+        print('Divisible by 2 and 3')
+    else:
+        print('Divisible by 2 and not by 3')
+elif x%3 == 0:
+    print('Divisible by 3 and not by 2')
