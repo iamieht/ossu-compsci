66845d40a5c1a0e0eec9d6a98b6ff9ec5f15c0d3
Iván E. Hernández T <iamieht@gmail.com>
2022-03-17 08:54:33 +0100

    Update guessAndCheck.py
    support for negative integers

--- Intro_2_ComputerScience/MITx6.00.1x/Week1/guessAndCheck.py
+++ Intro_2_ComputerScience/MITx6.00.1x/Week1/guessAndCheck.py
@@ -1,10 +1,12 @@
 x = int(input('Enter an integer: '))
 ans = 0
 
-while ans ** 3 < x:
+while ans ** 3 < abs(x):
     ans = ans + 1
 
-if ans ** 3 != x:
+if ans ** 3 != abs(x):
     print(str(x) + ' is not a perfect cube')
 else:
+    if x < 0:
+        ans = - ans
     print('Cube root of ' + str(x) + ' is ' + str(ans))
