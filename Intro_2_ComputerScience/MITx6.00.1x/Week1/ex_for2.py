60bc760f7611aa5b9da7e3142c6e757e408ae8eb
Iván E. Hernández T <iamieht@gmail.com>
2022-03-17 08:24:40 +0100

    Create ex_for2.py
    Exercise: for exercise 2

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week1/ex_for2.py
@@ -0,0 +1,13 @@
+'''
+Convert the following code into code that uses a for loop.
+
+prints Hello!
+prints 10
+prints 8
+prints 6
+prints 4
+prints 2
+'''
+print('Hello!')
+for num in range(10,0,-2):
+    print(num)
