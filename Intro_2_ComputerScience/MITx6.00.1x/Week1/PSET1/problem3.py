6b462bff8bfe733c02ad253fcfc018ea9ec68886
Iván E. Hernández T <iamieht@gmail.com>
2022-03-17 15:44:25 +0100

    Create problem3.py
    Problem 3

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week1/PSET1/problem3.py
@@ -0,0 +1,10 @@
+'''
+Assume s is a string of lower case characters.
+
+Write a program that prints the longest substring of s in which the letters occur in alphabetical order. For example, if s = 'azcbobobegghakl', then your program should print
+
+Longest substring in alphabetical order is: beggh
+In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print
+
+Longest substring in alphabetical order is: abc
+'''
