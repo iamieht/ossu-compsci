eb4095828b4581a0d6732485c935e4b69500c8b5
Iván E. Hernández T <iamieht@gmail.com>
2022-03-17 08:12:20 +0100

    Create ex_while2.py
    Exercise: while exercise 2

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week1/ex_while2.py
@@ -0,0 +1,15 @@
+'''
+Convert the following into code that uses a while loop.
+
+prints Hello!
+prints 10
+prints 8
+prints 6
+prints 4
+prints 2
+'''
+print('Hello!')
+num = 10
+while num >= 2:
+    print(num)
+    num -= 2
