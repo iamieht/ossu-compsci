2f28116fe29766122f7512ebafa469022066645b
Iván E. Hernández T <iamieht@gmail.com>
2022-03-17 16:19:00 +0100

    Update problem3.py
    Problem 3 final

--- Intro_2_ComputerScience/MITx6.00.1x/Week1/PSET1/problem3.py
+++ Intro_2_ComputerScience/MITx6.00.1x/Week1/PSET1/problem3.py
@@ -8,3 +8,29 @@ In the case of ties, print the first substring. For example, if s = 'abcbcd', th
 
 Longest substring in alphabetical order is: abc
 '''
+# Test Cases
+#s = 'abcbcd' #Longest substring in alphabetical order is: abc
+#s = 'azcbobobegghakl' #Longest substring in alphabetical order is: beggh
+#s = 'vecmxxlytamcwzvicguop' #Longest substring in alphabetical order is: cmxx
+#s = 'yeycakrnkffztrb' #Longest substring in alphabetical order is: akr
+#s = 'gfpjvjnbeygaeopxghb' #Longest substring in alphabetical order is: aeopx
+s = 'abcdefghijklmnopqrstuvwxyz' #Longest substring in alphabetical order is: abcdefghijklmnopqrstuvwxyz
+# Main Prog
+currentLongest = s[0]
+longest = ''
+
+for idx in range(1,len(s)):
+    if s[idx] >= s[idx-1]:
+        currentLongest += s[idx]
+        if len(currentLongest) > len(longest):
+            longest = currentLongest
+    else:
+        if len(currentLongest) > len(longest):
+            longest = currentLongest
+            currentLongest = s[idx]
+        else:
+            currentLongest = s[idx]
+
+
+
+print('Longest substring in alphabetical order is:', longest)
