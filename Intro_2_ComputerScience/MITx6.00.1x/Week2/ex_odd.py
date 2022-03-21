e261d6a2436c9ec924320f58d7af78936e1f8bbe
Iván E. Hernández T <iamieht@gmail.com>
2022-03-21 16:30:35 +0100

    Create ex_odd.py
    Exercise: odd

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week2/ex_odd.py
@@ -0,0 +1,11 @@
+def odd(x):
+    '''
+    x: number
+    returns True when number is odd and False otherwise
+    '''
+    return x%2 != 0
+
+# Test Cases
+print(odd(-16)) #False
+print(odd(-15)) #True
+print(odd(-62)) #False
