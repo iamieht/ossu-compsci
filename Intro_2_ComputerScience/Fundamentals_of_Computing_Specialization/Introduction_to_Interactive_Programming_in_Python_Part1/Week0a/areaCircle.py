9e562cf4f99e9aca76462c6c016e994fd78cae83
Iván E. Hernández T <iamieht@gmail.com>
2022-03-23 19:19:17 +0100

    Create areaCircle.py
    Area of Circle

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0a/areaCircle.py
@@ -0,0 +1,12 @@
+# Compute the area of a circle, given the length of its radius.
+
+###################################################
+# Circle area formula
+# Student should enter statement on the next line.
+
+print 3.14 * 8**2
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#200.96
