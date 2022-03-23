79cd603a54a10b362acf8596e6d425be7202a663
Iván E. Hernández T <iamieht@gmail.com>
2022-03-23 19:27:39 +0100

    Create pointDistance.py
    Point Distance

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0a/pointDistance.py
@@ -0,0 +1,15 @@
+# Compute the distance between the points (x0, y0) and (x1, y1).
+
+###################################################
+# Distance formula
+# Student should enter statement on the next line.
+
+# Hint: You need to use the power operation ** .
+
+print ((2 - 5) ** 2 + (2 - 6) ** 2)**0.5
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#5.0
