b957ac6221dd2e8ded896867bbb3dbfa7852dd2d
Iván E. Hernández T <iamieht@gmail.com>
2022-03-23 19:22:39 +0100

    Create nameTag.py
    Name Tag

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0a/nameTag.py
@@ -0,0 +1,13 @@
+# Compute a name tag, given the first and last name.
+
+###################################################
+# Name tag formula
+# Student should enter statement on the next line.
+
+print 'My Name is' + ' Joe' + ' Warren'
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#My name is Joe Warren.
