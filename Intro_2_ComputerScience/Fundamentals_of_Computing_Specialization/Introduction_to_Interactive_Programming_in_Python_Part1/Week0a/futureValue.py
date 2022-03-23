c8c09aac02234470fc1494a2f2a6e11ec63cc9c1
Iván E. Hernández T <iamieht@gmail.com>
2022-03-23 19:21:18 +0100

    Create futureValue.py
    Future Value

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0a/futureValue.py
@@ -0,0 +1,13 @@
+# Compute the future value of a given present value, annual rate, and number of years.
+
+###################################################
+# Future value formula
+# Student should enter statement on the next line.
+
+print 1000 * (1 + 0.01*7)**10
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#1967.15135729
