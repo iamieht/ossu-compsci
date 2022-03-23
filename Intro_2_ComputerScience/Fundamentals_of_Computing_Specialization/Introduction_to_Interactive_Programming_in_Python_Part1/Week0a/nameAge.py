06de391ba6140b472833c460dfa6f00eeadcecdc
Iván E. Hernández T <iamieht@gmail.com>
2022-03-23 19:23:53 +0100

    Create nameAge.py
    Name and Age

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0a/nameAge.py
@@ -0,0 +1,12 @@
+# Compute the statement about a person's name and age, given the person's name and age.
+
+###################################################
+# Name and age formula
+# Student should enter statement on the next line.
+
+print 'Joe Warren is ' + str(52) + ' years old.'
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#Joe Warren is 52 years old.
