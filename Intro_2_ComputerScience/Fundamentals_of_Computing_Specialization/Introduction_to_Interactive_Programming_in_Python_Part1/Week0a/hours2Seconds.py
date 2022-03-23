085e7b0f7130678b95f62d912a0a2a4cb0253878
Iván E. Hernández T <iamieht@gmail.com>
2022-03-23 19:11:12 +0100

    Create hours2Seconds.py
    Hours to Seconds

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0a/hours2Seconds.py
@@ -0,0 +1,15 @@
+# Compute the number of seconds in a given number of hours, minutes, and seconds.
+
+###################################################
+# Hours, minutes, and seconds to seconds conversion formula
+# Student should enter statement on the next line.
+# Write a Python statement that calculates and prints the number of seconds in 7 hours, 21 minutes and 37 seconds
+
+print 7 * 3600 + 21 * 60 + 37
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#26497
