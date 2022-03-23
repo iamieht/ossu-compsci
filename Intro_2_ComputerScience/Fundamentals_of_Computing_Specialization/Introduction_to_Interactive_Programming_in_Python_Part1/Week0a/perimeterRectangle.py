bfa5858d9091b2131f6f7bcafc04362fc9f13d42
Iván E. Hernández T <iamieht@gmail.com>
2022-03-23 19:13:52 +0100

    Create perimeterRectangle.py
    Perimeter of Rectangle

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0a/perimeterRectangle.py
@@ -0,0 +1,18 @@
+# Compute the length of a rectangle's perimeter, given its width and height.
+
+###################################################
+# Rectangle perimeter formula
+# Student should enter statement on the next line.
+# The perimeter of a rectangle is 2w + 2h, 
+# where w and h are the lengths of its sides. 
+# Write a Python statement that calculates and prints the length in inches of the perimeter of a rectangle 
+# with sides of length 4 and 7 inches
+
+print 2 * 4 + 2 * 7
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#22
