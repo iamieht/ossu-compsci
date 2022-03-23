88825b4e0a5816e73af39dd1e63ca0aa5e3338d8
Iván E. Hernández T <iamieht@gmail.com>
2022-03-23 19:17:57 +0100

    Create circumferenceCircle.py
    Circumference of Circle

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0a/circumferenceCircle.py
@@ -0,0 +1,13 @@
+# Compute the circumference of a circle, given the length of its radius.
+
+###################################################
+# Circle circumference formula
+# Student should enter statement on the next line.
+
+print 2 * 3.14 * 8
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#50.24
