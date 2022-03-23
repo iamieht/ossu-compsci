d68cbb5dc54a1381631d2764b97943d72b142bd5
Iván E. Hernández T <iamieht@gmail.com>
2022-03-23 19:05:00 +0100

    Create miles2Feet.py
    Miles to Feet

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0a/miles2Feet.py
@@ -0,0 +1,13 @@
+# Compute the number of feet corresponding to a number of miles.
+
+###################################################
+# Miles to feet conversion formula
+# Student should enter statement on the next line.
+
+print 5280 * 13
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#68640
