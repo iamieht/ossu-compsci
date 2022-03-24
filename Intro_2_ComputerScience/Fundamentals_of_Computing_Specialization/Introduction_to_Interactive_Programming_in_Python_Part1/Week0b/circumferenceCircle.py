bc1d53d52bf41f5bbc4435d5cb301e490a17a76f
Iván E. Hernández T <iamieht@gmail.com>
2022-03-24 15:18:53 +0100

    Create circumferenceCircle.py
    Circumference of Circle

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0b/circumferenceCircle.py
@@ -0,0 +1,45 @@
+# Compute the circumference of a circle, given the length of its radius.
+
+###################################################
+# Tests
+# Student should uncomment ONLY ONE of the following at a time.
+PI = 3.14
+
+# Test 1 - Select the following lines and use ctrl+shift+k to uncomment.
+radius = 8
+
+
+# Test 2 - Select the following lines and use ctrl+shift+k to uncomment.
+#radius = 3
+
+
+# Test 3 - Select the following lines and use ctrl+shift+k to uncomment.
+#radius = 12.9
+
+
+###################################################
+# Circle circumference formula
+# Student should enter formula on the next line.
+circumference = 2 * PI * radius
+
+
+###################################################
+# Test output
+# Student should not change this code.
+
+print "A circle with a radius of " + str(radius),
+print "inches has a circumference of " + str(circumference) + " inches."
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+# Test 1 output:
+#A circle with a radius of 8 inches has a circumference of 50.24 inches.
+
+# Test 2 output:
+#A circle with a radius of 3 inches has a circumference of 18.84 inches.
+
+# Test 3 output:
+#A circle with a radius of 12.9 inches has a circumference of 81.012 inches.
