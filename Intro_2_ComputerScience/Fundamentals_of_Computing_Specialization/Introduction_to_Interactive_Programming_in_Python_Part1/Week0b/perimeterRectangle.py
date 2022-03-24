5232da2af4c9f47a5ebe5b5c258f6251d61a3d60
Iván E. Hernández T <iamieht@gmail.com>
2022-03-24 15:17:23 +0100

    Create perimeterRectangle.py
    Perimeter Rectangle

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0b/perimeterRectangle.py
@@ -0,0 +1,47 @@
+# Compute the length of a rectangle's perimeter, given its width and height.
+
+###################################################
+# Tests
+# Student should uncomment ONLY ONE of the following at a time.
+
+# Test 1 - Select the following lines and use ctrl+shift+k to uncomment.
+width = 4
+height = 7
+
+
+# Test 2 - Select the following lines and use ctrl+shift+k to uncomment.
+#width = 7
+#height = 4
+
+
+# Test 3 - Select the following lines and use ctrl+shift+k to uncomment.
+#width = 10
+#height = 10
+
+
+###################################################
+# Rectangle perimeter formula
+# Student should enter formula on the next line.
+
+perimeter = width * 2 + height * 2
+
+###################################################
+# Test output
+# Student should not change this code.
+
+print "A rectangle " + str(width) + " inches wide and " + str(height),
+print "inches high has a perimeter of " + str(perimeter) + " inches."
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+# Test 1 output:
+#A rectangle 4 inches wide and 7 inches high has a perimeter of 22 inches.
+
+# Test 2 output:
+#A rectangle 7 inches wide and 4 inches high has a perimeter of 22 inches.
+
+# Test 3 output:
+#A rectangle 10 inches wide and 10 inches high has a perimeter of 40 inches.
