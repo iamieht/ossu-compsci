30a64a90aedfef3cad30cfd7b713e24e2bd129fa
Iván E. Hernández T <iamieht@gmail.com>
2022-03-24 15:19:36 +0100

    Create areaCircle.py
    Area of Circle

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0b/areaCircle.py
@@ -0,0 +1,45 @@
+# Compute the area of a circle, given the length of its radius.
+
+###################################################
+# Tests
+# Student should uncomment ONLY ONE of the following at a time.
+PI = 3.14
+
+# Test 1 - Select the following lines and use ctrl+shift+k to uncomment.
+radius = 8
+
+
+# Test 2 - Select the following lines and use ctrl+shift+k to uncomment.
+#radius = 3
+
+
+# Test 3 - Select the following lines and use ctrl+shift+k to uncomment.
+#radius = 12.9
+
+
+###################################################
+# Circle area formula
+# Student should enter formula on the next line.
+area = PI * radius ** 2
+
+
+###################################################
+# Test output
+# Student should not change this code.
+
+print "A circle with a radius of " + str(radius),
+print "inches has an area of " + str(area) + " square inches."
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+# Test 1 output:
+#A circle with a radius of 8 inches has an area of 200.96 square inches.
+
+# Test 2 output:
+#A circle with a radius of 3 inches has an area of 28.26 square inches.
+
+# Test 3 output:
+#A circle with a radius of 12.9 inches has an area of 522.5274 square inches.
