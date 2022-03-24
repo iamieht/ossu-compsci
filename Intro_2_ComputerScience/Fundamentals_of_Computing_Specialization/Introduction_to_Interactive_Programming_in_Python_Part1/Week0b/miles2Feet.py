ef2082f5d758e04e9064c490015afe56be357792
Iván E. Hernández T <iamieht@gmail.com>
2022-03-24 15:13:13 +0100

    Create miles2Feet.py
    miles 2 feet

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0b/miles2Feet.py
@@ -0,0 +1,43 @@
+# Compute the number of feet corresponding to a number of miles.
+
+###################################################
+# Tests
+# Student should uncomment ONLY ONE of the following at a time.
+
+# Test 1 - Select the following lines and use ctrl+shift+k to uncomment.
+#miles = 13
+
+
+# Test 2 - Select the following lines and use ctrl+shift+k to uncomment.
+#miles = 57
+
+
+# Test 3 - Select the following lines and use ctrl+shift+k to uncomment.
+miles = 82.67
+
+
+###################################################
+# Miles to feet conversion formula
+# Student should enter formula on the next line.
+feet = miles * 5280
+
+
+###################################################
+# Test output
+# Student should not change this code.
+
+print str(miles) + " miles equals " + str(feet) + " feet."
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+# Test 1 output:
+#13 miles equals 68640 feet.
+
+# Test 2 output:
+#57 miles equals 300960 feet.
+
+# Test 3 output:
+#82.67 miles equals 436497.6 feet.
