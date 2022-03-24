7f816f694ffa512979d0fa5a6ade27f7c2994d82
Iván E. Hernández T <iamieht@gmail.com>
2022-03-24 13:31:59 +0100

    Create fib_efficient.py
    Fibonacci efficient

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week3/fib_efficient.py
@@ -0,0 +1,10 @@
+def fib_efficient(n, d):
+    if n in d:
+        return d[n]
+    else:
+        ans = fib_efficient(n-1, d) + fib_efficient(n-2, d)
+        d[n] = ans
+        return ans
+
+d = {1:1, 2:2}
+print(fib_efficient(6, d))
