207f5d27a1bd1e6cd39a29b8df5c942758c818a8
Iván E. Hernández T <iamieht@gmail.com>
2022-03-24 15:20:45 +0100

    Create nameTag.py
    Name Tag

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0b/nameTag.py
@@ -0,0 +1,46 @@
+# Compute a name tag, given the first and last name.
+
+###################################################
+# Tests
+# Student should uncomment ONLY ONE of the following at a time.
+
+# Test 1 - Select the following lines and use ctrl+shift+k to uncomment.
+first_name = "Joe"
+last_name = "Warren"
+
+
+# Test 2 - Select the following lines and use ctrl+shift+k to uncomment.
+#first_name = "Scott"
+#last_name = "Rixner"
+
+
+# Test 3 - Select the following lines and use ctrl+shift+k to uncomment.
+#first_name = "John"
+#last_name = "Greiner"
+
+
+###################################################
+# Name tag formula
+# Student should enter formula on the next line.
+name_tag = "My name is " + first_name + " " + last_name + "."
+
+
+###################################################
+# Test output
+# Student should not change this code.
+
+print name_tag
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+# Test 1 output:
+#My name is Joe Warren.
+
+# Test 2 output:
+#My name is Scott Rixner.
+
+# Test 3 output:
+#My name is John Greiner.
