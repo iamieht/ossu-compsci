5b7edad45e81301301da87020e58a732b43cf7f4
Iván E. Hernández T <iamieht@gmail.com>
2022-03-24 15:21:15 +0100

    Create nameAge.py
    name and age

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week0b/nameAge.py
@@ -0,0 +1,45 @@
+# Compute the statement about a person's name and age, given the person's name and age.
+
+###################################################
+# Tests
+# Student should uncomment ONLY ONE of the following at a time.
+
+# Test 1 - Select the following lines and use ctrl+shift+k to uncomment.
+name = "Joe Warren"
+age = 52
+
+
+# Test 2 - Select the following lines and use ctrl+shift+k to uncomment.
+#name = "Scott Rixner"
+#age = 40
+
+
+# Test 3 - Select the following lines and use ctrl+shift+k to uncomment.
+#name = "John Greiner"
+#age = 46
+
+
+###################################################
+# Name and age formula
+# Student should enter formula on the next line.
+statement = name + " is " + str(age) + " years old."
+
+
+###################################################
+# Test output
+# Student should not change this code.
+
+print statement
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+# Test 1 output:
+#Joe Warren is 52 years old.
+
+# Test 2 output:
+#Scott Rixner is 40 years old.
+
+# Test 3 output:
+#John Greiner is 46 years old.
