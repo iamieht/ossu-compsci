0dc1dbe01e6aaf42464030847ce8b60d42b7f059
Iván E. Hernández T <iamieht@gmail.com>
2022-03-26 19:45:19 +0100

    Update powerball.py

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week1a/powerball.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week1a/powerball.py
@@ -6,7 +6,7 @@
 import random
 
 def powerball():
-    print "Today's numbers are " + str(random.randrange(1,60)) + ', ' + str(random.randrange(1,60)) + ', ' + str(random.randrange(1,60)) + ', ' + str(random.randrange(1,60)) + ', and ' + str(random.randrange(1,36))
+    print "Today's numbers are " + str(random.randrange(1,60)) + ', ' + str(random.randrange(1,60)) + ', ' + str(random.randrange(1,60)) + ', ' + str(random.randrange(1,60)) + ', and ' + str(random.randrange(1,60)) + '.' + 'The Powerball number is' + str(random.randrange(1,36)) + '.'
 
 
 	
