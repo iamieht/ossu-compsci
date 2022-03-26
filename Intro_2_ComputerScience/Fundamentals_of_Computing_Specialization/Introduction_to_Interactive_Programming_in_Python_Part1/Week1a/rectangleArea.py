d3c3cb5e11173f748c2b8d9773b29383d170c5cd
Iván E. Hernández T <iamieht@gmail.com>
2022-03-26 19:17:46 +0100

    Create rectangleArea.py
    area of rectangle

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week1a/rectangleArea.py
@@ -0,0 +1,30 @@
+# Compute the area of a rectangle, given its width and height.
+
+###################################################
+# Rectangle area formula
+# Student should enter function on the next lines.
+def rectangle_area(width, hight):
+    return width * height
+
+
+###################################################
+# Tests
+# Student should not change this code.
+
+def test(width, height):
+	print "A rectangle " + str(width) + " inches wide and " + str(height),
+	print "inches high has an area of",
+	print str(rectangle_area(width, height)) + " square inches."
+
+test(4, 7)
+test(7, 4)
+test(10, 10)
+
+	
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#A rectangle 4 inches wide and 7 inches high has an area of 28 square inches.
+#A rectangle 7 inches wide and 4 inches high has an area of 28 square inches.
+#A rectangle 10 inches wide and 10 inches high has an area of 100 square inches.
