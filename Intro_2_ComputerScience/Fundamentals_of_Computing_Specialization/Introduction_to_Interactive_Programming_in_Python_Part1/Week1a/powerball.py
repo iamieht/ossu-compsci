b3c0278bec8eceebbb1e580446ac513fc7c1f69a
Iván E. Hernández T <iamieht@gmail.com>
2022-03-26 19:39:03 +0100

    Create powerball.py
    powerball

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week1a/powerball.py
@@ -0,0 +1,19 @@
+# Compute and print powerball numbers.
+
+###################################################
+# Powerball function
+# Student should enter function on the next lines.
+import random
+
+def powerball():
+    print "Today's numbers are " + str(random.randrange(1,60)) + ', ' + str(random.randrange(1,60)) + ', ' + str(random.randrange(1,60)) + ', ' + str(random.randrange(1,60)) + ', and ' + str(random.randrange(1,36))
+
+
+	
+###################################################
+# Tests
+# Student should not change this code.
+	
+powerball()
+powerball()
+powerball()
