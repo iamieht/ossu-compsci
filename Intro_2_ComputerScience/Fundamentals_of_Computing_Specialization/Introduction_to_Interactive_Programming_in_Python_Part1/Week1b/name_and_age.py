612bde968f13ce325f1eeb4efd9aa40256132ebb
Iván E. Hernández T <iamieht@gmail.com>
2022-03-28 13:17:17 +0200

    Create name_and_age.py
    name and age

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week1b/name_and_age.py
@@ -0,0 +1,32 @@
+# Compute the statement about a person's name and age, given the person's name and age.
+
+###################################################
+# Name and age formula
+# Student should enter function on the next lines.
+def name_and_age(name, age):
+    if age < 0:
+        return "Error: Invalid age"
+    else:
+        return str(name) + " is " + str(age) + " years old."
+
+###################################################
+# Tests
+# Student should not change this code.
+
+def test(name, age):
+    """Tests the name_and_age function."""
+    
+    print name_and_age(name, age)
+    
+test("Joe Warren", 52)
+test("Scott Rixner", 40)
+test("John Greiner", -46)
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#Joe Warren is 52 years old.
+#Scott Rixner is 40 years old.
+#Error: Invalid age
