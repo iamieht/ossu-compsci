65d07181110ad4e1d7fd38591662df9e584e814b
Iván E. Hernández T <iamieht@gmail.com>
2022-03-28 12:25:33 +0200

    Create is_leap_year.py
    is_leap_year

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week1b/is_leap_year.py
@@ -0,0 +1,39 @@
+# Compute whether the given year is a leap year.
+
+###################################################
+# Is leapyear formula
+# Student should enter function on the next lines.
+def is_leap_year(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
+###################################################
+# Tests
+# Student should not change this code.
+
+def test(year):
+    """Tests the is_leapyear function."""
+    if is_leap_year(year):
+        print year, "is a leap year."
+    else:
+        print year, "is not a leap year."
+
+test(2000)
+test(1996)
+test(1800)
+test(2013)
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#2000 is a leap year.
+#1996 is a leap year.
+#1800 is not a leap year.
+#2013 is not a leap year.
