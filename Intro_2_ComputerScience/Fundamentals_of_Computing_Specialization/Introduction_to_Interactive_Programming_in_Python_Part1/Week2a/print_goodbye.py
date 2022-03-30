fd3d3bdb0887a6927491717e834deac837571a52
Iván E. Hernández T <iamieht@gmail.com>
2022-03-30 15:43:47 +0200

    Create print_goodbye.py
    Week2a - print_goodbye()

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week2a/print_goodbye.py
@@ -0,0 +1,32 @@
+# Printing "Goodbye" with a local message variable
+
+###################################################
+# Student should enter function on the next lines.
+def print_goodbye():
+    message = "Goodbye"
+    print message
+
+
+###################################################
+# Tests
+
+message = "Hello"
+print message
+print_goodbye()
+print message
+
+message = "Ciao"
+print message
+print_goodbye()
+print message
+
+
+###################################################
+# Output
+
+#Hello
+#Goodbye
+#Hello
+#Ciao
+#Goodbye
+#Ciao
