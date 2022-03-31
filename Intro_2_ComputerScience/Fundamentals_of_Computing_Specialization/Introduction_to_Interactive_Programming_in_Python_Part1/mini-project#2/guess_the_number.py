e16876948f0ca96e2f2f0a10bc6ddec7dbbcd61c
Iván E. Hernández T <iamieht@gmail.com>
2022-03-31 13:20:52 +0200

    Create guess_the_number.py
    Mini-project # 2 - Guess the number

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/mini-project#2/guess_the_number.py
@@ -0,0 +1,44 @@
+# template for "Guess the number" mini-project
+# input will come from buttons and an input field
+# all output for the game will be printed in the console
+
+
+
+# helper function to start and restart the game
+def new_game():
+    # initialize global variables used in your code here
+
+    # remove this when you add your code    
+    pass
+
+
+# define event handlers for control panel
+def range100():
+    # button that changes the range to [0,100) and starts a new game 
+    
+    # remove this when you add your code    
+    pass
+
+def range1000():
+    # button that changes the range to [0,1000) and starts a new game     
+    
+    pass
+    
+def input_guess(guess):
+    # main game logic goes here	
+    
+    # remove this when you add your code
+    pass
+
+    
+# create frame
+
+
+# register event handlers for control elements and start frame
+
+
+# call new_game 
+new_game()
+
+
+# always remember to check your completed program against the grading rubric
