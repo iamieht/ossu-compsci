75c03ae9b913a911bd8621f9d522acb66690ed58
Iván E. Hernández T <iamieht@gmail.com>
2022-03-31 14:15:15 +0200

    Update guess_the_number.py
    main logic implemented

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/mini-project#2/guess_the_number.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/mini-project#2/guess_the_number.py
@@ -17,19 +17,22 @@ def new_game():
     global secret_number, remaining_guesses
     secret_number = random.randrange(0, num_range)
     print "New game. Range is from 0 to", num_range
-    print "Number of remaining guesses is", remaining_guesses
+    print "Number of remaining guesses is"
+    print
 
 
 # define event handlers for control panel
 def range100():
     # button that changes the range to [0,100) and starts a new game 
-    global num_range = 100
-	new_game()
+    global num_range
+    num_range = 100
+    new_game()
 
 def range1000():
     # button that changes the range to [0,1000) and starts a new game     
     global num_range
     num_range = 1000
+    new_game()
     
     
 def input_guess(guess):
@@ -43,19 +46,22 @@ def input_guess(guess):
     
     if guess == secret_number:
         print "Correct"
+        
     elif guess > secret_number:
-        print "Lower"
+        print "Lower!"
+        print
     else:
-        print "Higher"
+        print "Higher!"
+        print
 
     
 # create frame
-frame = simplegui.create_frame("Guess the number", 100, 100)
+frame = simplegui.create_frame("Guess the number", 200, 200)
 
 # register event handlers for control elements and start frame
-frame.add_input("Enter a number:", input_guess, 100)
-frame.add_button("Range is [0, 100)", range_100)
-frame.add_button("Range is [0, 1000)", range_1000)
+frame.add_button("Range is [0, 100)", range100, 150)
+frame.add_button("Range is [0, 1000)", range1000, 150)
+frame.add_input("Enter a number:", input_guess, 150)
 frame.start()
 
 
