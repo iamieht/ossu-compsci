0317dfb57d4cfcb5246ceb51f5fc8841e4c89785
Iván E. Hernández T <iamieht@gmail.com>
2022-03-31 13:53:30 +0200

    Update guess_the_number.py
    Basic logic

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/mini-project#2/guess_the_number.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/mini-project#2/guess_the_number.py
@@ -1,15 +1,21 @@
 # template for "Guess the number" mini-project
 # input will come from buttons and an input field
 # all output for the game will be printed in the console
+import simplegui
+import random
+import math
 
+#Global Variables
+num_range = 100
 
 
 # helper function to start and restart the game
 def new_game():
-    # initialize global variables used in your code here
-
-    # remove this when you add your code    
-    pass
+    '''
+    Generates a new game
+    '''
+    global secret_number
+    secret_number = random.randrange(0, num_range)
 
 
 # define event handlers for control panel
@@ -25,16 +31,28 @@ def range1000():
     pass
     
 def input_guess(guess):
-    # main game logic goes here	
+    '''
+    Main game logis
+    guess: string
+    check if guess == secret_number
+    '''
+    guess = int(guess)
+    print "Guess was", int(guess)
     
-    # remove this when you add your code
-    pass
+    if guess == secret_number:
+        print "Correct"
+    elif guess > secret_number:
+        print "Lower"
+    else:
+        print "Higher"
 
     
 # create frame
-
+frame = simplegui.create_frame("Guess the number", 100, 100)
 
 # register event handlers for control elements and start frame
+frame.add_input("Enter a number:", input_guess, 100)
+frame.start()
 
 
 # call new_game 
