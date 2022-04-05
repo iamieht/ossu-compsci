65a2003ccc6d72b82a33dd3ffff16591fc936ca1
Iván E. Hernández T <iamieht@gmail.com>
2022-04-05 11:08:32 +0200

    Update stopwatch.py
    create timer

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/mini-project#3/stopwatch.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/mini-project#3/stopwatch.py
@@ -1,7 +1,7 @@
-# template for "Stopwatch: The Game"
-
+# "Stopwatch: The Game"
+import simplegui
 # define global variables
-
+time = 0
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
@@ -12,18 +12,24 @@ def format(t):
 
 
 # define event handler for timer with 0.1 sec interval
-
+def tick():
+    global time
+    time += 1
+    print time
 
 # define draw handler
 
     
 # create frame
+frame = simplegui.create_frame("Stopwatch: The Game", 300, 300)
 
-
+# create timer
+timer = simplegui.create_timer(10, tick)
 # register event handlers
 
 
-# start frame
-
+# start frame & timer
+frame.start()
+timer.start()
 
 # Please remember to review the grading rubric
