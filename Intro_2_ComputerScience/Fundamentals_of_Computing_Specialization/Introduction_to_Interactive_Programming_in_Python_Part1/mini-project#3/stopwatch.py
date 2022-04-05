486e5434e15af64ff18abebc6a70be2d8bd34952
Iván E. Hernández T <iamieht@gmail.com>
2022-04-05 11:15:31 +0200

    Update stopwatch.py
    add start / stop / reset buttons

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/mini-project#3/stopwatch.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/mini-project#3/stopwatch.py
@@ -9,27 +9,36 @@ def format(t):
     pass
     
 # define event handlers for buttons; "Start", "Stop", "Reset"
+def start():
+    timer.start()
 
+def stop():
+    timer.stop()
+
+def reset():
+    global time
+    time = 0
 
 # define event handler for timer with 0.1 sec interval
 def tick():
     global time
     time += 1
-    print time
 
 # define draw handler
-
+def draw(canvas):
+    canvas.draw_text(str(time), [300/2, 300/2], 36, "White")
     
 # create frame
 frame = simplegui.create_frame("Stopwatch: The Game", 300, 300)
 
 # create timer
 timer = simplegui.create_timer(10, tick)
-# register event handlers
 
+# register event handlers
+frame.set_draw_handler(draw)
+frame.add_button("Start", start, 50)
+frame.add_button("Stop", stop, 50)
+frame.add_button("Reset", reset, 50)
 
 # start frame & timer
 frame.start()
-timer.start()
-
-# Please remember to review the grading rubric
