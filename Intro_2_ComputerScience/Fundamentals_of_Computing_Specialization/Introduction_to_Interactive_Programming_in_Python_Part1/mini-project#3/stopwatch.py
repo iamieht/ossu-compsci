1250380dfc915da9ddbfe13c9b1580009b2fa8c5
Iván E. Hernández T <iamieht@gmail.com>
2022-04-05 10:57:05 +0200

    Create stopwatch.py
    mini-project#3: Stopwatch: The Game

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/mini-project#3/stopwatch.py
@@ -0,0 +1,29 @@
+# template for "Stopwatch: The Game"
+
+# define global variables
+
+
+# define helper function format that converts time
+# in tenths of seconds into formatted string A:BC.D
+def format(t):
+    pass
+    
+# define event handlers for buttons; "Start", "Stop", "Reset"
+
+
+# define event handler for timer with 0.1 sec interval
+
+
+# define draw handler
+
+    
+# create frame
+
+
+# register event handlers
+
+
+# start frame
+
+
+# Please remember to review the grading rubric
