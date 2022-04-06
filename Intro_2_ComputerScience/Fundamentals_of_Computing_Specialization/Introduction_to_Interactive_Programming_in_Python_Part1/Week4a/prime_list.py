4e4c5d6d25fdd7283a682d6294cd30e57e85e0d1
Iván E. Hernández T <iamieht@gmail.com>
2022-04-06 15:14:12 +0200

    Create prime_list.py
    Week4a primes list

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week4a/prime_list.py
@@ -0,0 +1,13 @@
+# Prime number lists
+
+###################################################
+# Student should enter code below
+primes = [2, 3, 5, 7, 11, 13]
+print primes[1], primes[3], primes[-1]
+
+
+###################################################
+# Expected output
+
+#3 7 13
+
