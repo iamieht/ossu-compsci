42284c3ffe45bf734d2498d3f4e48c615f08f3e4
Iván E. Hernández T <iamieht@gmail.com>
2022-04-06 15:16:54 +0200

    Create list_copy.py
    list copy

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week4a/list_copy.py
@@ -0,0 +1,12 @@
+# List reference problem
+
+###################################################
+# Student should enter code below
+
+a = [5, 3, 1, -1, -3, 5]
+b = list(a)
+b[0] = 0
+print a,b
+
+###################################################
+# Explanation
