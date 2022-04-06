318c0014d4728fa03792ba50c0b00a5bfa0a86bc
Iván E. Hernández T <iamieht@gmail.com>
2022-04-06 15:19:46 +0200

    Create vector_addition.py
    vector addition

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/Week4a/vector_addition.py
@@ -0,0 +1,25 @@
+# Vector addition function
+
+###################################################
+# Student should enter code below
+def add_vector(v, w):
+    return [v[0] + w[0], v[1] + w[1]]
+
+
+
+###################################################
+# Test
+
+print add_vector([4, 3], [0, 0])
+print add_vector([1, 2], [3, 4])
+print add_vector([2, 3], [-6, -3])
+
+
+
+###################################################
+# Output
+
+#[4, 3]
+#[4, 6]
+#[-4, 0]
+
