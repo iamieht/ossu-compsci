261a9cf0bade8a58ef71c084f8cfe065ba4c2ac4
Iván E. Hernández T <iamieht@gmail.com>
2022-04-08 13:59:32 +0200

    Update pong.py
    draw_ball + move across the screen

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/mini-project#4/pong.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Python_Part1/mini-project#4/pong.py
@@ -13,6 +13,8 @@ HALF_PAD_WIDTH = PAD_WIDTH / 2
 HALF_PAD_HEIGHT = PAD_HEIGHT / 2
 LEFT = False
 RIGHT = True
+ball_pos = [WIDTH//2, HEIGHT//2]
+ball_vel = [1,0]
 
 # initialize ball_pos and ball_vel for new bal in middle of table
 # if direction is RIGHT, the ball's velocity is upper right, else upper left
@@ -27,7 +29,7 @@ def new_game():
 
 def draw(canvas):
     global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel
- 
+    
         
     # draw mid line and gutters
     canvas.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
@@ -35,8 +37,11 @@ def draw(canvas):
     canvas.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
         
     # update ball
+    ball_pos[0] = ball_pos[0] + ball_vel[0]
+    ball_pos[1] = ball_pos[1] + ball_vel[1]
             
     # draw ball
+    canvas.draw_circle(ball_pos, BALL_RADIUS, 5, "White", "White")
     
     # update paddle's vertical position, keep paddle on the screen
     
