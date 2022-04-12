1112a01bfa9cfbd1e2731e6948e04cca111bf4d3
Iván E. Hernández T <iamieht@gmail.com>
2022-04-12 20:58:18 +0200

    Create ex_inSet.py
    ex - intSet

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week5/ex_inSet.py
@@ -0,0 +1,44 @@
+class intSet(object):
+    """An intSet is a set of integers
+    The value is represented by a list of ints, self.vals.
+    Each int in the set occurs in self.vals exactly once."""
+
+    def __init__(self):
+        """Create an empty set of integers"""
+        self.vals = []
+
+    def insert(self, e):
+        """Assumes e is an integer and inserts e into self""" 
+        if not e in self.vals:
+            self.vals.append(e)
+
+    def member(self, e):
+        """Assumes e is an integer
+           Returns True if e is in self, and False otherwise"""
+        return e in self.vals
+
+    def remove(self, e):
+        """Assumes e is an integer and removes e from self
+           Raises ValueError if e is not in self"""
+        try:
+            self.vals.remove(e)
+        except:
+            raise ValueError(str(e) + ' not found')
+
+    def intersect(self, other):
+        newIntSet = intSet()
+
+        for e in self.vals:
+            if other.member(e):
+                newIntSet.insert(e)
+
+        return newIntSet
+
+    def __len__(self):
+        return len(self.vals)
+
+    def __str__(self):
+        """Returns a string representation of self"""
+        self.vals.sort()
+        return '{' + ','.join([str(e) for e in self.vals]) + '}'
+
