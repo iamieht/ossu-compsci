b8e03f4679f994caca4dff8328e516c236dfb7a9
Iván E. Hernández T <iamieht@gmail.com>
2022-04-12 20:46:37 +0200

    Create intSet.py
    intSet

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week5/intSet.py
@@ -0,0 +1,19 @@
+class intSet(object):
+    def __init__(self):
+        self.vals = []
+    def insert(self, e):
+        if not e in self.vals:
+            self.vals.append(e)
+    def member(self, e):
+        return e in self.vals
+    def remove(self,e):
+        try:
+            self.vals.remove(e)
+        except:
+            raise ValueError(str(e) + ' not found')
+    def __str__(self):
+        self.vals.sort()
+        result = ''
+        for e in self.vals:
+            result = result + str(e) + ','
+        return '{' + result[:-1] + '}'
