8f2658666fc6ba2e900d1cb3eb7828556ea8bbd5
Iván E. Hernández T <iamieht@gmail.com>
2022-04-13 10:48:02 +0200

    Create animal.py
    animal class + subclasses

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week5/animal.py
@@ -0,0 +1,59 @@
+class Animal(object):
+    def __init__(self, age):
+        self.age = age
+        self.name = None
+
+    def get_age(self):
+        return self.age
+
+    def get_name(self):
+        return self.name
+    
+    def set_age(self, newage):
+        self.age = newage
+
+    def set_name(self, newname=""):
+        self.name = newname
+
+    def __str__(self):
+        return 'animal:' + str(self.name) + ':' + str(self.age)
+
+class Cat(Animal):
+    def speak(self):
+        print('meow')
+
+    def __str__(self):
+        return 'cat:' + str(self.name) + ':' + str(self.age)
+
+class Rabit(Animal):
+    def speak(self):
+        print('meep')
+
+    def __str__(self):
+        return 'rabbit:' + str(self.name) + ':' + str(self.age)
+
+class Person(Animal):
+    def __init__(self, name, age):
+        Animal.__init__(self, age)
+        Animal.set_name(self, name)
+        self.friends = []
+
+    def get_friends(self):
+        return self.friends
+
+    def add_friend(self, fname):
+        if fname not in self.friends:
+            self.friends.append(fname)
+
+    def speak(self):
+        print('hello')
+
+    def age_diff(self, other):
+        diff = self.get_age() - other.get_age()
+        if self.age > other.age:
+            print(self.name, 'is', diff, 'years older than', other.name)
+        else:
+            print(self.name, 'is', -diff, 'years younger than', other.name)
+
+    def __str__(self):
+        return 'person:' + str(self.name) + ':' + str(self.age)
