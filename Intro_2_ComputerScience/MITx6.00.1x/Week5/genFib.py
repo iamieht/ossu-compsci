fc9d9bf7c5fd376bf7b90cc5e6dd110c1226175c
Iván E. Hernández T <iamieht@gmail.com>
2022-04-14 14:47:58 +0200

    Create genFib.py
    generator Fibonacci

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week5/genFib.py
@@ -0,0 +1,9 @@
+def genFib():
+    fibn_1 = 1
+    fibn_2 = 0
+    while True:
+        # fib(n) = fib(n-1) + fib(n-2)
+        next = fibn_1 + fibn_2
+        yield next
+        fibn_2 = fibn_1
+        fibn_1 = next
