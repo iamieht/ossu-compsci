bab71bc27c93a8152607bdcf378747207a3b1139
Iván E. Hernández T <iamieht@gmail.com>
2022-04-14 14:31:20 +0200

    Create gradebook.py
    class gradebook

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week5/gradebook.py
@@ -0,0 +1,39 @@
+from multiprocessing.sharedctypes import Value
+
+
+class Grades(object):
+    '''A mapping from students to a list of grades'''
+    def __init__(self):
+        '''create empty grade book'''
+        self.students = []      # list of Student objects
+        self.grades = {}        # maps idNum -> list of grades
+        self.isSorted = True    # true if self.students is sorted
+
+
+    def addStudent(self, student):
+        '''Assumes: student is of type Student
+           Add student to the grade book'''
+
+        if student in self.students:
+            raise ValueError('Duplicate student')
+        
+        self.students.append(student)
+        self.grades[student.getIdNum()] = []
+        self.isSorted = False
+
+    def addGrade(self, student, grade):
+        '''Assumes: grade is a float
+           Add grade to the list of grades for student'''
+        
+        try:
+            self.grades[student.getIdNum()].append(grade)
+        except KeyError:
+            raise ValueError('Student not in grade book')
+
+    def getGrades(self, student):
+        '''Return a list of grades for student'''
+
+        try:
+            return self.grades[student.getIdNum()][:]
+        except KeyError:
+            raise ValueError('Student not in grade book')
