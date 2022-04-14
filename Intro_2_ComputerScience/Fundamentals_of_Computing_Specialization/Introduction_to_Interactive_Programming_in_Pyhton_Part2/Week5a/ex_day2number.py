e91f8154f38a0484eb3dfaff58302b95a4115d07
Iván E. Hernández T <iamieht@gmail.com>
2022-04-14 20:08:42 +0200

    Create ex_day2number.py
    day to number

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Pyhton_Part2/Week5a/ex_day2number.py
@@ -0,0 +1,32 @@
+# Day to number problem
+
+###################################################
+# Student should enter code below
+
+day_list = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+def day_to_number(day):
+    return day_list.index(day)
+
+###################################################
+# Test data
+
+print day_to_number("Sunday")
+print day_to_number("Monday")
+print day_to_number("Tuesday")
+print day_to_number("Wednesday")
+print day_to_number("Thursday")
+print day_to_number("Friday")
+print day_to_number("Saturday")
+
+###################################################
+# Sample output
+
+#0
+#1
+#2
+#3
+#4
+#5
+#6
+
