9673e7b2dbcb3c2f1a9de613c93906676bb3fe8a
Iván E. Hernández T <iamieht@gmail.com>
2022-04-14 14:20:11 +0200

    Update hand.py
    update method

--- Intro_2_ComputerScience/MITx6.00.1x/Week5/hand.py
+++ Intro_2_ComputerScience/MITx6.00.1x/Week5/hand.py
@@ -82,8 +82,27 @@ class Hand(object):
         word: string
         returns: Boolean (if the word was or was not made)
         """
-        # Your code here
-        raise NotImplementedError()
+        new_hand = self.hand.copy()
+
+        for letter in word:
+            try:
+                new_hand[letter] -= 1
+            except KeyError:
+                # if 'letter' isn't in the hand, we can't make the word from this hand.
+                return False
+
+        for letter in new_hand.keys():
+            # If any of the letter counts of the new hand are less than zero after the
+            # update, then we can't make the word from this hand.
+            if new_hand[letter] < 0:
+                return False
+
+        # If we've gotten to here, we must be able to make the word from this hand.
+        # Set self.hand to the new, updated hand and return True.
+        self.hand = new_hand
+        return True
+        
+
 
     
 myHand = Hand(7)
