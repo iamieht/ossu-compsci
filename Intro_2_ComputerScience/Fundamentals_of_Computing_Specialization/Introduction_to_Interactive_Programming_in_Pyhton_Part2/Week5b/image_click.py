e263b73ef9f5af2869261cc737d8c436a497dbdb
Iván E. Hernández T <iamieht@gmail.com>
2022-04-18 10:01:20 +0200

    Update image_click.py
    move image via mouse click

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Pyhton_Part2/Week5b/image_click.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Pyhton_Part2/Week5b/image_click.py
@@ -15,19 +15,24 @@ test_image = simplegui.load_image("http://commondatastorage.googleapis.com/codes
 # image dimesions
 image_size = (95,93)
 
+# image_pos
+image_pos = [WIDTH//2,HEIGHT//2]
+
 # mouseclick handler
 def click(pos):
-    pass
+    image_pos[0] = pos[0]
+    image_pos[1] = pos[1]
     
 # draw handler
 def draw(canvas):
-    canvas.draw_image(test_image, (image_size[0]//2, image_size[1]//2), image_size, (WIDTH//2,HEIGHT//2), image_size)
+    canvas.draw_image(test_image, (image_size[0]//2, image_size[1]//2), image_size, image_pos, image_size)
 
     
 # create frame and register draw handler    
 frame = simplegui.create_frame("Test image", WIDTH, HEIGHT)
 frame.set_canvas_background("Gray")
 frame.set_draw_handler(draw)
+frame.set_mouseclick_handler(click)
 
 
 # start frame
