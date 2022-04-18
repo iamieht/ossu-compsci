3b3c97eb0f004a52bbd8e1df96cb8cc8b166bb43
Iván E. Hernández T <iamieht@gmail.com>
2022-04-18 09:21:10 +0200

    Create name_lookup.py
    name lookup

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Pyhton_Part2/Week5b/name_lookup.py
@@ -0,0 +1,25 @@
+# Day to number dictionary problem
+
+###################################################
+# Student should enter code below
+name_lookup = {"Joe":"Warren",
+              "Scott":"Rixner",
+              "John":"Greiner",
+              "Stephen":"Wong"}
+
+###################################################
+# Test data
+
+print name_lookup["Joe"]
+print name_lookup["Scott"]
+print name_lookup["John"]
+print name_lookup["Stephen"]
+
+
+###################################################
+# Sample output
+
+#Warren
+#Rixner
+#Greiner
+#Wong
