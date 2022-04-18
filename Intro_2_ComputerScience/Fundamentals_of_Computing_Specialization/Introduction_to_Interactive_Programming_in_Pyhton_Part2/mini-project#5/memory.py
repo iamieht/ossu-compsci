d3da67e8e4bde963d5df9d31a3735ebf4a0693fb
Iván E. Hernández T <iamieht@gmail.com>
2022-04-18 14:13:22 +0200

    Update memory.py
    draw card deck and green rectangles

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Pyhton_Part2/mini-project#5/memory.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Pyhton_Part2/mini-project#5/memory.py
@@ -4,10 +4,13 @@ import simplegui
 import random
 
 card_deck = list(range(0,8)) + list(range(0,8))
+card_deck_exposed = 16*[False]
+
 
 # helper function to initialize globals
 def new_game():
-    pass  
+    global card_deck
+    random.shuffle(card_deck)
 
      
 # define event handlers
@@ -18,8 +21,15 @@ def mouseclick(pos):
                         
 # cards are logically 50x100 pixels in size    
 def draw(canvas):
-    for idx in range(len(card_deck)):
-        canvas.draw_text(str(card_deck[idx]), (idx*50+25, 60), 36, "White")
+    for idx in range(1,len(card_deck) + 1):
+        if card_deck_exposed[idx-1]:
+            canvas.draw_text(str(card_deck[idx-1]), (idx*50-35, 65), 48, "White")
+        else:
+            a = (idx * 50) - 50
+            b = 0
+            c = idx * 50
+            d = 100
+            canvas.draw_polygon([(a, b), (a, d), (c, d), (c, b)], 2, "Black", "Green")
 
 
 # create frame and add a button and labels
@@ -34,5 +44,7 @@ frame.set_draw_handler(draw)
 # get things rolling
 new_game()
 frame.start()
+
+
 
 # CodeSkulptor: https://py2.codeskulptor.org/#user49_Gzo0xsOhp7_0.py
