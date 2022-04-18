5d94484117a7bb87b73e42a5bbc129668345295e
Iván E. Hernández T <iamieht@gmail.com>
2022-04-18 09:18:47 +0200

    Create day_to_number.py
    day to number

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Pyhton_Part2/Week5b/day_to_number.py
@@ -0,0 +1,34 @@
+# Day to number dictionary problem
+
+###################################################
+# Student should enter code below
+day_to_number = {"Sunday":0,
+                "Monday":1,
+                "Tuesday":2,
+                "Wednesday":3,
+                "Thursday":4,
+                "Friday":5,
+                "Saturday":6}
+
+
+###################################################
+# Test data
+
+print day_to_number["Sunday"]
+print day_to_number["Monday"]
+print day_to_number["Tuesday"]
+print day_to_number["Wednesday"]
+print day_to_number["Thursday"]
+print day_to_number["Friday"]
+print day_to_number["Saturday"]
+
+###################################################
+# Sample output
+
+#0
+#1
+#2
+#3
+#4
+#5
+#6
