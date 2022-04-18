1129938ff5236378ad1d6061e1c9e0cd4d01c7d1
Iván E. Hernández T <iamieht@gmail.com>
2022-04-18 14:37:26 +0200

    Update memory.py
    main game logic completed

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Pyhton_Part2/mini-project#5/memory.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Pyhton_Part2/mini-project#5/memory.py
@@ -4,23 +4,46 @@ import simplegui
 import random
 
 card_deck = list(range(0,8)) + list(range(0,8))
-card_deck_exposed = 16*[False]
+
 
 
 # helper function to initialize globals
 def new_game():
-    global card_deck
+    global card_deck, state, card_deck_exposed
+    card_deck_exposed = 16*[False]
     random.shuffle(card_deck)
+    state = 0
 
      
 # define event handlers
 def mouseclick(pos):
-    # add game state logic here
-    pass
+    global state, card1_idx, card2_idx
+    idx = pos[0] // 50
+    
+    if state == 0:
+        card_deck_exposed[idx] = True
+        card1_idx = idx
+        state = 1
+    elif state == 1:
+        if not card_deck_exposed[idx]:
+            card_deck_exposed[idx] = True
+            card2_idx = idx
+            state = 2
+    else:
+        if not card_deck_exposed[idx]:
+            card_deck_exposed[idx] = True
+            
+            if card_deck[card1_idx] != card_deck[card2_idx]:
+                card_deck_exposed[card1_idx] = False
+                card_deck_exposed[card2_idx] = False
+                
+            card1_idx = idx
+            state = 1
     
                         
 # cards are logically 50x100 pixels in size    
 def draw(canvas):
+    global card_deck_exposed
     for idx in range(1,len(card_deck) + 1):
         if card_deck_exposed[idx-1]:
             canvas.draw_text(str(card_deck[idx-1]), (idx*50-35, 65), 48, "White")
@@ -44,6 +67,8 @@ frame.set_draw_handler(draw)
 # get things rolling
 new_game()
 frame.start()
+
+
 
 
 
