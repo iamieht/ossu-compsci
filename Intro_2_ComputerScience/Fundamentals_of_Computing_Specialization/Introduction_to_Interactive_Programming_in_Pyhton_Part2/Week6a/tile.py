6be5946ff089dfc04c9335af52a46dab7338c77d
Iván E. Hernández T <iamieht@gmail.com>
2022-04-19 15:02:12 +0200

    Create tile.py
    Tile class

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Pyhton_Part2/Week6a/tile.py
@@ -0,0 +1,30 @@
+# Create an instance of a Tile object 
+
+# definition of empty Tile class (use pass in the body)
+class Tile:
+    
+    pass
+    
+    
+#################################################
+# Student adds code where appropriate    
+my_tile = Tile()
+your_tile = Tile()
+    
+    
+###################################################
+# Testing code 
+
+print my_tile
+print type(my_tile)
+print your_tile
+print type(your_tile)
+
+
+####################################################
+# Output of testing code
+
+#<__main__.Tile object>
+#<class '__main__.Tile'>
+#<__main__.Tile object>
+#<class '__main__.Tile'>
