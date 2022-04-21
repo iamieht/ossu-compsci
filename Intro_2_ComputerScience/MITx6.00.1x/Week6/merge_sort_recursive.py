db3e98132caf74091255ef7c6e71a0059a573da1
Iván E. Hernández T <iamieht@gmail.com>
2022-04-21 12:45:18 +0200

    Create merge_sort_recursive.py
    merge sort recursive

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week6/merge_sort_recursive.py
@@ -0,0 +1,9 @@
+import merge_sort
+def merge_sort(L):
+    if len(L) < 2:
+        return L[:]
+    else:
+        middle = len(L) // 2
+        left = merge_sort(L[:middle])
+        right = merge_sort(L[middle:])
+        return merge_sort(left, right)
