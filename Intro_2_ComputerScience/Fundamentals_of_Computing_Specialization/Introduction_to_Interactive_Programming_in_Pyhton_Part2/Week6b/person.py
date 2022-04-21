652aa9c3e42f0e342c3ff7bb03b3d9bc38350cdf
Iván E. Hernández T <iamieht@gmail.com>
2022-04-21 20:23:03 +0200

    Create person.py
    Person class

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Introduction_to_Interactive_Programming_in_Pyhton_Part2/Week6b/person.py
@@ -0,0 +1,54 @@
+# Implementation of Person class
+
+
+#################################################
+# Student adds code where appropriate
+
+# definition of Person class
+class Person:
+    
+    def __init__(self, first, last, year):
+        self.first_name = first
+        self.last_name = last
+        self.birth_year = year
+        
+    def full_name(self):
+        return str(self.first_name) + ' ' + str(self.last_name)
+    
+    def age(self, current_year):
+        return current_year - self.birth_year
+    
+    def __str__(self):
+        return "The person's name is " + self.full_name() + ". " + "Their birth year is " + str(self.birth_year) 
+ 
+    
+###################################################
+# Testing code
+
+joe = Person("Joe", "Warren", 1961)
+john = Person("John", "Greiner", 1966)
+stephen = Person("Stephen", "Wong", 1960)
+scott = Person("Scott", "Rixner", 1987)  
+
+print joe
+print john
+print stephen
+print scott
+
+print joe.age(2013)
+print scott.age(2013)   # yeah, right ;)
+print john.full_name()
+print stephen.full_name()
+
+
+####################################################
+# Output of testing code - results of __str__ method may vary
+
+#The person's name is Joe Warren. Their birth year is 1961
+#The person's name is John Greiner. Their birth year is 1966
+#The person's name is Stephen Wong. Their birth year is 1960
+#The person's name is Scott Rixner. Their birth year is 1987
+#52
+#26
+#John Greiner
+#Stephen Wong
