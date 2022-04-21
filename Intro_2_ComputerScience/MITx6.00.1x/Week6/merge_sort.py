7cc160337b36b65746c1f7ed02d05c3e841b671b
Iván E. Hernández T <iamieht@gmail.com>
2022-04-21 12:42:59 +0200

    Create merge_sort.py
    merge sort

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week6/merge_sort.py
@@ -0,0 +1,18 @@
+def merge(left, right):
+    result = []
+    i,j = 0,0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    
+    while (i < len(left)):
+        result.append(left[i])
+        i += 1
+    while (j < len(right)):
+        result.append(right[j])
+        j += 1
+    return result
