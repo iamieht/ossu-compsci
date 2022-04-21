36d1fedb60cf7370d9fed27c4032a09472df7607
Iván E. Hernández T <iamieht@gmail.com>
2022-04-21 12:25:27 +0200

    Create bisection_search2.py
    bisection search 2

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week6/bisection_search2.py
@@ -0,0 +1,20 @@
+def bisect_search2(L, e):
+    def bisect_search_helper(L, e, low, high):
+        if high == low:
+            return L[low] == e
+        mid = (low + high) // 2
+        if L[mid] == e:
+            return True
+        elif L[mid] > e:
+            if low == mid: #nothing left to search
+                return False
+            else:
+                return bisect_search_helper(L, e, low, mid - 1)
+        else:
+            return bisect_search_helper(L, e, mid + 1, high)
+    
+    if len(L) == 0:
+        return False
+    else:
+        return bisect_search_helper(L, e, 0, len(L) - 1)
+    
