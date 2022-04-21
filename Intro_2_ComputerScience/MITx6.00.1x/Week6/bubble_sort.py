e33c505c197366c976c27f3f70ddff0a5c3bd112
Iván E. Hernández T <iamieht@gmail.com>
2022-04-21 12:29:59 +0200

    Create bubble_sort.py
    bubble sort

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week6/bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(L):
+    swap = False
+    while not swap:
+        swap = True
+        for j in range(1, len(L)):
+            if L[j-1] > L[j]:
+                swap = False
+                temp = L[j]
+                L[j] = L[j-1]
+                L[j-1] = temp
