3fbe00f98753875b645788f25565a149ffefabaa
Iván E. Hernández T <iamieht@gmail.com>
2022-04-22 12:54:00 +0200

    Create plot_example.py
    retire

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week7/plot_example.py
@@ -0,0 +1,8 @@
+def retire(monthly, rate, terms):
+    savings = [0]
+    base = [0]
+    mRate = rate/12
+    for i in range(terms):
+        base += [i]
+        savings += [savings[-1]*(1 + mRate) + monthly]
+    return base, savings
