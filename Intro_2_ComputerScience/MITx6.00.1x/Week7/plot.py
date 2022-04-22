1ffa297523547f3e1f01d56e7914c5d08bb702bb
Iván E. Hernández T <iamieht@gmail.com>
2022-04-22 12:06:09 +0200

    Create plot.py
    plot

---
+++ Intro_2_ComputerScience/MITx6.00.1x/Week7/plot.py
@@ -0,0 +1,20 @@
+import pylab as plt
+
+mySamples = []
+myLinear = []
+myQuadratic = []
+myCubic = []
+myExponential = []
+
+for i in range(0, 30):
+    mySamples.append(i)
+    myLinear.append(i)
+    myQuadratic.append(i**2)
+    myCubic.append(i**3)
+    myExponential.append(1.5**i)
+
+#first trial
+plt.plot(mySamples, myLinear)
+plt.plot(mySamples, myQuadratic)
+plt.plot(mySamples, myCubic)
+plt.plot(mySamples, myExponential)
