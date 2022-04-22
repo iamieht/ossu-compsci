904f48f2128f5a191a4ffff215946c8d80ba420f
Iván E. Hernández T <iamieht@gmail.com>
2022-04-22 12:58:55 +0200

    Update plot_example.py
    displayRetireWMonthlies

--- Intro_2_ComputerScience/MITx6.00.1x/Week7/plot_example.py
+++ Intro_2_ComputerScience/MITx6.00.1x/Week7/plot_example.py
@@ -1,3 +1,5 @@
+import pylab as plt
+
 def retire(monthly, rate, terms):
     savings = [0]
     base = [0]
@@ -6,3 +8,13 @@ def retire(monthly, rate, terms):
         base += [i]
         savings += [savings[-1]*(1 + mRate) + monthly]
     return base, savings
+
+def displayRetireWMonthlies(monthlies, rate, terms):
+    plt.figure('retireMonth')
+    plt.clf()
+    for monthly in monthlies:
+        xvals, yvals = retire(monthly, rate. terms)
+        plt.plot(xvals, yvals, label = 'retire:' + str(monthly))
+        plt.legend(loc = 'upper left')
+
+displayRetireWMonthlies([500,600,700,800,900,1000,1100], .05, 40*12)
