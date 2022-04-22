9a35404df8ce5b316d51dcf2b35259a41cb45c89
Iván E. Hernández T <iamieht@gmail.com>
2022-04-22 13:18:20 +0200

    Update plot_example.py

--- Intro_2_ComputerScience/MITx6.00.1x/Week7/plot_example.py
+++ Intro_2_ComputerScience/MITx6.00.1x/Week7/plot_example.py
@@ -29,10 +29,16 @@ def displayRetireWMonthsAndRates(monthlies, rates, terms):
     plt.figure('retireBoth')
     plt.clf()
     plt.xlim(30*12, 40*12)
-    for monthly in monthlies:
-        for rate in rates:
+    monthLabels = ['r', 'b', 'g', 'k']
+    rateLabels = ['-', 'o', '--']
+    for i in range(len(monthlies)):
+        monthly = monthlies[i]
+        monthLabel = monthLabels[i%len(monthLabels)]
+        for j in range(len(rates)):
+            rate = rates[j]
+            rateLabel = rateLabels[j%len(rateLabels)]
             xvals, yvals = retire(monthly, rate, terms)
-            plt.plot(xvals, yvals, label='retire:'+str(monthly)+':'+str(int(rate*100)))
+            plt.plot(xvals, yvals, monthLabel+rateLabel, label='retire:'+str(monthly)+':'+str(int(rate*100)))
             plt.legend(loc = 'upper left')
 
 displayRetireWMonthlies([500,600,700,800,900,1000,1100], .05, 40*12)
