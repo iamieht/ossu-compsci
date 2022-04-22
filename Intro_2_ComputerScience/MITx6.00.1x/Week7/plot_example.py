3268f222c8021b84673dfd7158e630f518df6059
Iván E. Hernández T <iamieht@gmail.com>
2022-04-22 13:03:48 +0200

    Update plot_example.py
    displayRetireWRates

--- Intro_2_ComputerScience/MITx6.00.1x/Week7/plot_example.py
+++ Intro_2_ComputerScience/MITx6.00.1x/Week7/plot_example.py
@@ -17,4 +17,13 @@ def displayRetireWMonthlies(monthlies, rate, terms):
         plt.plot(xvals, yvals, label = 'retire:' + str(monthly))
         plt.legend(loc = 'upper left')
 
+def displayRetireWRates(month, rates, terms):
+    plt.figure('retireRate')
+    plt.clf()
+    for rate in rates:
+        xvals, yvals = retire(month, rate, terms)
+        plt.plot(xvals, yvals, label = 'retire:'+str(month)+':'+str(int(rate*100)))
+        plt.legend(loc = 'upper left')
+
 displayRetireWMonthlies([500,600,700,800,900,1000,1100], .05, 40*12)
+displayRetireWRates(800, [.03, .05, .07], 40*12)
