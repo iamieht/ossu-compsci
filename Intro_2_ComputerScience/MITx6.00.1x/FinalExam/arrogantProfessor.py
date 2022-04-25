3969d2f2651071e70b3abf89e3001a4fc616b80e
Iván E. Hernández T <iamieht@gmail.com>
2022-04-25 11:51:03 +0200

    Create arrogantProfessor.py
    arrogant Professor class

---
+++ Intro_2_ComputerScience/MITx6.00.1x/FinalExam/arrogantProfessor.py
@@ -0,0 +1,22 @@
+class Person(object):     
+    def __init__(self, name):         
+        self.name = name     
+    def say(self, stuff):         
+        return self.name + ' says: ' + stuff     
+    def __str__(self):         
+        return self.name  
+
+class Lecturer(Person):     
+    def lecture(self, stuff):         
+        return 'I believe that ' + Person.say(self, stuff)  
+
+class Professor(Lecturer): 
+    def say(self, stuff): 
+        return self.name + ' says: ' + self.lecture(stuff)
+
+class ArrogantProfessor(Professor): 
+    def say(self, stuff): 
+        return Person.say(self, self.lecture(stuff))
+
+    def lecture(self, stuff):
+        return 'It is obvious that ' + Person.say(self, stuff)
