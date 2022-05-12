ab1c9a209583604e3888aa6b013506ae3e7505e9
Iván E. Hernández T <iamieht@gmail.com>
2022-05-12 15:49:45 +0200

    Update 2048-merge.py
    clean up

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Principles_of_Computing_Part1/Week1/mini-project#1/2048-merge.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Principles_of_Computing_Part1/Week1/mini-project#1/2048-merge.py
@@ -1,87 +1,37 @@
 """
 Merge function for 2048 game.
 """
-def zeros_end(aList):
+def zeros_end(alist):
     """
     Function that moves non-zero elements of a list to
     the beginning and zeros at the end.
     Returns a new list.
     """
-    newList = []
+    new_list = []
     zeros = 0
-    for element in aList:
+    for element in alist:
         if element == 0:
             zeros += 1
         else:
-            newList.append(element)
+            new_list.append(element)
             
-    newList.extend(zeros*[0])
-    return newList
-
-def sublist(aList, num):
-    """
-    Function that creates a sublist group by number of elements
-    num: Int
-    """
-    aList2 = list()
-    for i in range(0, len(aList), num):
-        aList2.append(aList[i:i+num])
-    
-    return aList2
-
-def pair_elements(aList):
-    """
-    Group similar elements in pairs
-    """
-    pass
-        
-        
-#    for idx in range(len(aList)):
-#        if aList[idx] == aList[idx+1]:
-#            result.append(aList[idx:idx+1])
-#            
-        
-        
+    new_list.extend(zeros*[0])
+    return new_list
 
 def merge(line):
     """
     Function that merges a single row or column in 2048.
     """
-    merged_list = zeros_end(line)
-#    merged_list = sublist(merged_list, 2)
+    new_line = zeros_end(line)
     result = []
-    zeros = len(line)
     
-    for idx in range(len(merged_list)-1):
-        if merged_list[idx] == merged_list[idx+1]:
-            merged_list[idx] *= 2
-            merged_list[idx+1] = 0
+    for idx in range(len(new_line)-1):
+        if new_line[idx] == new_line[idx+1]:
+            new_line[idx] *= 2
+            new_line[idx+1] = 0
             
-    result = zeros_end(merged_list)
-    
-    
-    
-#    for element in merged_list:
-#        if len(element) > 1:
-#            if element[0] == element[1]:
-#                result.append(element[0] + element[1])
-#            else:
-#                result.extend(element)
-#        else:
-#            result.extend(element)
-                
-#    result.extend((zeros-len(result))*[0])
+    result = zeros_end(new_line)
     return result
-    
-#    for idx in range(0,len(new_line)-1,2):
-#        if new_line[idx] == new_line[idx+1]:
-#            new_line2.append(new_line[idx]+new_line[idx+1])
-#        else:
-#            new_line2.append(new_line[idx])
-#            
-#    return new_line2
-
-
 
 
 print "Test Cases zeros_end"
@@ -91,13 +41,7 @@ print zeros_end([2, 2, 0, 0])
 print zeros_end([2, 2, 2, 2, 2])
 print zeros_end([8, 16, 16, 8])
 print "=========================="
-print "Test sublist"
-print sublist([2, 0, 2, 4], 2)
-print sublist([0, 0, 2, 2], 2)
-print sublist([2, 2, 0, 0], 2)
-print sublist([2, 2, 2, 2, 2], 2)
-print sublist([8, 16, 16, 8], 2)
-print "=========================="
+
 print "Test Cases merge"
 print merge([2, 0, 2, 4])
 print merge([0, 0, 2, 2])
@@ -105,4 +49,4 @@ print merge([2, 2, 0, 0])
 print merge([2, 2, 2, 2, 2])
 print merge([8, 16, 16, 8])
 
-#CodeSkulptor: https://py2.codeskulptor.org/#user49_zcnxRetw1j_2.py
+#CodeSkulptor: https://py2.codeskulptor.org/#user49_zcnxRetw1j_4.py
