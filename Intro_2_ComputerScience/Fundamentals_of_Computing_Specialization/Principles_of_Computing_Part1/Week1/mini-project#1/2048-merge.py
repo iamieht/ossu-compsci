6316ecdcd3af45791dd16f776f79795ef2881d9e
Iván E. Hernández T <iamieht@gmail.com>
2022-05-12 15:36:15 +0200

    Update 2048-merge.py
    merge is working but need to clean up the code

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Principles_of_Computing_Part1/Week1/mini-project#1/2048-merge.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Principles_of_Computing_Part1/Week1/mini-project#1/2048-merge.py
@@ -28,13 +28,50 @@ def sublist(aList, num):
         aList2.append(aList[i:i+num])
     
     return aList2
+
+def pair_elements(aList):
+    """
+    Group similar elements in pairs
+    """
+    pass
+        
+        
+#    for idx in range(len(aList)):
+#        if aList[idx] == aList[idx+1]:
+#            result.append(aList[idx:idx+1])
+#            
+        
         
 
 def merge(line):
     """
     Function that merges a single row or column in 2048.
     """
-    pass
+    merged_list = zeros_end(line)
+#    merged_list = sublist(merged_list, 2)
+    result = []
+    zeros = len(line)
+    
+    for idx in range(len(merged_list)-1):
+        if merged_list[idx] == merged_list[idx+1]:
+            merged_list[idx] *= 2
+            merged_list[idx+1] = 0
+            
+    result = zeros_end(merged_list)
+    
+    
+    
+#    for element in merged_list:
+#        if len(element) > 1:
+#            if element[0] == element[1]:
+#                result.append(element[0] + element[1])
+#            else:
+#                result.extend(element)
+#        else:
+#            result.extend(element)
+                
+#    result.extend((zeros-len(result))*[0])
+    return result
     
 #    for idx in range(0,len(new_line)-1,2):
 #        if new_line[idx] == new_line[idx+1]:
@@ -60,13 +97,12 @@ print sublist([0, 0, 2, 2], 2)
 print sublist([2, 2, 0, 0], 2)
 print sublist([2, 2, 2, 2, 2], 2)
 print sublist([8, 16, 16, 8], 2)
-#print ""
-#print "Test Cases merge"
-#print merge([2, 0, 2, 2])
-#print merge([2, 0, 2, 4])
-#print merge([0, 0, 2, 2])
-#print merge([2, 2, 0, 0])
-#print merge([2, 2, 2, 2, 2])
-#print merge([8, 16, 16, 8])
+print "=========================="
+print "Test Cases merge"
+print merge([2, 0, 2, 4])
+print merge([0, 0, 2, 2])
+print merge([2, 2, 0, 0])
+print merge([2, 2, 2, 2, 2])
+print merge([8, 16, 16, 8])
 
-#CodeSkulptor: https://py2.codeskulptor.org/#user49_zcnxRetw1j_1.py
+#CodeSkulptor: https://py2.codeskulptor.org/#user49_zcnxRetw1j_2.py
