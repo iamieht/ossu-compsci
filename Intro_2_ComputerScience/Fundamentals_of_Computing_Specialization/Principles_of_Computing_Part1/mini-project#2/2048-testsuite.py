ed46f28f30acdf0d5fbe17423d2e65f724b7b8be
Iván E. Hernández T <iamieht@gmail.com>
2022-05-20 20:46:13 +0200

    Update 2048-testsuite.py
    add some tests

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Principles_of_Computing_Part1/mini-project#2/2048-testsuite.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Principles_of_Computing_Part1/mini-project#2/2048-testsuite.py
@@ -19,8 +19,11 @@ def run_suite(game_class):
 
     # test the initial configuration of the board using the str method
     suite.run_test(str(game_class), str(game_class), "Test #0: init")
+    suite.run_test(str(game_class.reset()), str(game_class.reset()), "Test #1: reset()")
+    suite.run_test(str(game_class.new_tile()), str(game_class.new_tile()), "Test #2: new_tile()")
     
     # report number of tests and failures
     suite.report_results()
 
-#CodeSkulptor: https://py2.codeskulptor.org/#user49_rrK9yzg2Bo_6.py
+
+#CodeSkulptor: https://py2.codeskulptor.org/#user49_rrK9yzg2Bo_9.py
