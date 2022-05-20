3d3b4d84495542ad1c3befbfeb396c8ca9c95b00
Iván E. Hernández T <iamieht@gmail.com>
2022-05-20 19:23:21 +0200

    Create 2048-testsuite.py
    test suite initial commit

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Principles_of_Computing_Part1/mini-project#2/2048-testsuite.py
@@ -0,0 +1,24 @@
+"""
+Template testing suite for 2048
+"""
+
+import poc_simpletest
+
+def run_suite(game_class):
+    """
+    Some informal testing code
+    """
+    
+    # create a TestSuite object
+    suite = poc_simpletest.TestSuite()    
+    
+    # create a game
+    #game = game_class(h,g)
+    
+    # add tests using suite.run_test(....) here
+
+    # test the initial configuration of the board using the str method
+#    suite.run_test(str(game), str([0]), "Test #0: init")
+    
+    # report number of tests and failures
+    suite.report_results()
