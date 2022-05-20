212ce676fd8c23c77b33a1235015a30354eb62e6
Iván E. Hernández T <iamieht@gmail.com>
2022-05-20 20:46:17 +0200

    Update 2048.py

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Principles_of_Computing_Part1/mini-project#2/2048.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Principles_of_Computing_Part1/mini-project#2/2048.py
@@ -146,7 +146,7 @@ class TwentyFortyEight:
 #poc_2048_gui.run_gui(TwentyFortyEight(4, 4))
 
 #Test Suite
-import user49_rrK9yzg2Bo_6 as poc_2048_testsuite
+import user49_rrK9yzg2Bo_9 as poc_2048_testsuite
 poc_2048_testsuite.run_suite(TwentyFortyEight(4,4))
 
 
