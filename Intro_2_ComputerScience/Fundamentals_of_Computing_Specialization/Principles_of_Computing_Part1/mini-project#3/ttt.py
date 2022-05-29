dc9b1cd6b12be24a38e08612a5675217eacd2a7a
Iván E. Hernández T <iamieht@gmail.com>
2022-05-29 11:28:53 +0200

    Update ttt.py
    main function def

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Principles_of_Computing_Part1/mini-project#3/ttt.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Principles_of_Computing_Part1/mini-project#3/ttt.py
@@ -7,13 +7,44 @@ import poc_ttt_gui
 import poc_ttt_provided as provided
 
 # Constants for Monte Carlo simulator
-# You may change the values of these constants as desired, but
-#  do not change their names.
 NTRIALS = 1         # Number of trials to run
 SCORE_CURRENT = 1.0 # Score for squares played by the current player
 SCORE_OTHER = 1.0   # Score for squares played by the other player
     
 # Add your functions here.
+
+def mc_trial(board, player):
+    '''
+    This function takes a current board and the next player to move.
+    board mutates and nothing is returned
+    '''
+    pass
+
+def mc_update_scores(scores, board, player):
+    ''''
+    This function takes a grid of scores (a list of lists) with the same dimensions as the Tic-Tac-Toe board, 
+    a board from a completed game, and which player the machine player is. 
+    The function should score the completed board and update the scores grid. 
+    As the function updates the scores grid directly, it does not return anything
+    '''
+    pass
+
+def get_best_move(board, scores):
+    '''
+    This function takes a current board and a grid of scores. 
+    The function should find all of the empty squares with the maximum score and randomly return one of them as a (row, column) tuple. 
+    It is an error to call this function with a board that has no empty squares (there is no possible next move), 
+    so your function may do whatever it wants in that case. The case where the board is full will not be tested.
+    '''
+    pass
+
+def mc_move(board, player, trials):
+    '''
+    This function takes a current board, which player the machine player is, and the number of trials to run. 
+    The function should use the Monte Carlo simulation described above to return a move for the machine player 
+    in the form of a (row, column) tuple.
+    '''
+    pass
 
 
 
