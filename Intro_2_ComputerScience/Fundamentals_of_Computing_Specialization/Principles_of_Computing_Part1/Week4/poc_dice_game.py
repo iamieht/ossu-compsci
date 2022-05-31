55788c936fbce16979945dd4b19c8e36dc8d32d0
Iván E. Hernández T <iamieht@gmail.com>
2022-05-31 12:55:51 +0200

    Create poc_dice_game.py

---
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Principles_of_Computing_Part1/Week4/poc_dice_game.py
@@ -0,0 +1,69 @@
+"""
+Analyzing a simple dice game
+"""
+
+
+def gen_all_sequences(outcomes, length):
+    """
+    Iterative function that enumerates the set of all sequences of
+    outcomes of given length
+    """
+    
+    ans = set([()])
+    for dummy_idx in range(length):
+        temp = set()
+        for seq in ans:
+            for item in outcomes:
+                new_seq = list(seq)
+                new_seq.append(item)
+                temp.add(tuple(new_seq))
+        ans = temp
+    return ans
+
+# example for digits
+
+
+def max_repeats(seq):
+    """
+    Compute the maxium number of times that an outcome is repeated
+    in a sequence
+    """
+    item_count = [seq.count(item) for item in seq]
+    return max(item_count)
+
+
+def compute_expected_value():
+    """
+    Function to compute expected value of simple dice game
+    """
+    
+    all_rolls = gen_all_sequences([1, 2, 3, 4, 5, 6], 3)
+    results = [max_repeats(roll) for roll in all_rolls]
+    
+    expected_value = 0
+    for result in results:
+        if result == 2:
+            expected_value += 10 / 216.0
+        elif result == 3:
+            result == 3
+            expected_value += 200 / 216.0
+    return expected_value
+
+
+def run_test():
+    """
+    Testing code, note that the initial cost of playing the game
+    has been subtracted
+    """
+    outcomes = set([1, 2, 3, 4, 5, 6])
+    print "All possible sequences of three dice are"
+    print gen_all_sequences(outcomes, 3)
+    print
+    print "Test for max repeats"
+    print "Max repeat for (3, 1, 2) is", max_repeats((3, 1, 2))
+    print "Max repeat for (3, 3, 2) is", max_repeats((3, 3, 2))
+    print "Max repeat for (3, 3, 3) is", max_repeats((3, 3, 3))
+    print
+    print "Ignoring the initial $10, the expected value was $", compute_expected_value()
+    
+run_test()
