0ababe7ba0cc5c583f2b41e4522478e21c240d44
Iván E. Hernández T <iamieht@gmail.com>
2022-06-12 12:11:38 +0200

    Update cookie_clicker_simulator.py

--- Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Principles_of_Computing_Part1/mini-project#5/cookie_clicker_simulator.py
+++ Intro_2_ComputerScience/Fundamentals_of_Computing_Specialization/Principles_of_Computing_Part1/mini-project#5/cookie_clicker_simulator.py
@@ -228,16 +228,16 @@ def run_strategy(strategy_name, time, strategy):
     Run a simulation for the given time with one strategy.
     """
     state = simulate_clicker(provided.BuildInfo(), time, strategy)
-    print strategy_name, ":", state
+    print strategy_name, ":", state 
 
     # Plot total cookies over time
-
     # Uncomment out the lines below to see a plot of total cookies vs. time
     # Be sure to allow popups, if you do want to see it
+    # Also comment out "None" strategy in run function for the program to work correctly.
 
-    # history = state.get_history()
-    # history = [(item[0], item[3]) for item in history]
-    # simpleplot.plot_lines(strategy_name, 1000, 400, 'Time', 'Total Cookies', [history], True)
+    history = state.get_history()
+    history = [(item[0], item[3]) for item in history]
+    simpleplot.plot_lines(strategy_name, 1000, 400, 'Time', 'Total Cookies', [history], True)
 
 def run():
     """
